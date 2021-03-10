2a720a3224745c92ac6962e25814419da8414eb7
Nikolay <rasich16@gmail.com>
2021-03-10 19:35:38 +0200

    GET-запрос XMLHttpRequest

--- js/script.js
+++ js/script.js
@@ -1,5 +1,48 @@
 window.addEventListener('DOMContentLoaded', () => { 
 
-  
+  function req() {
+    const request = new XMLHttpRequest();
+    request.open('GET', 'http://localhost:3000/people');
+    request.setRequestHeader('Content-type', 'app;ication/json; charset=utf-8');
+    request.send();
+
+    // request.addEventListener('readystatechange', function() {
+      // if (request.readyState === 4 && request.status == 200) {
+    request.addEventListener('load', function() {
+      if (request.status == 200) {
+        let data = JSON.parse(request.response);
+        console.log(data);
+
+        data.forEach(item => {
+          let card = document.createElement('div');
+
+          card.classList.add('card');
+
+          let icon;
+          if (item.sex === 'male') {
+            icon = "icons/mars.png";
+          } else {
+            icon = "icons/female.png";
+          }
+
+          card.innerHTML = `
+            <img src="${item.photo}" alt="photo">
+            <div class="name">${item.name} ${item.surname}</div>
+            <div class="sex">
+              <img src=${icon} alt="male">
+            </div>
+            <div class="age">${item.age} age</div>
+          `;
+          document.querySelector('.app').appendChild(card);
+        });
+      } else {
+        console.error('Что-то пошло не так');
+      }
+    });
+
+    this.remove();
+  }
+
+  document.querySelector('button').addEventListener('click', req, {'once': true});
   
 });
