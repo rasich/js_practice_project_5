d4bbf695ad4940184784639c91119ed17add90cf
Nikolay <rasich16@gmail.com>
2021-03-11 17:49:47 +0200

    Отправка formData XMLHttpRequest

--- js/script.js
+++ js/script.js
@@ -5,41 +5,20 @@ window.addEventListener('DOMContentLoaded', () => {
     e.preventDefault();
 
     let formData = new FormData(form);
-    formData.append('id', Math.random());    
-    
-    let obj= {};
-    formData.forEach((value, key) => {
-      obj[key] = value;
-    });
 
-    axios.post("http://localhost:3000/people", obj);
+    const request = new XMLHttpRequest();
+    request.open('POST', './api.php');
+    request.send(formData);
+
+    request.addEventListener('load', function() {
+      if (request.status == 200) {
+        console.log(request.response);
+      } else {
+        console.error('Что-то пошло не так');
+      }
+    });
   }
 
   form.addEventListener('submit', (e) => req(e), {'once': true});
 
-  
-  // function createCards(response) {
-  //   response.forEach(item => {
-  //     let card = document.createElement('div');
-
-  //     card.classList.add('card');
-
-  //     let icon;
-  //     if (item.sex === 'male') {
-  //       icon = "icons/mars.png";
-  //     } else {
-  //       icon = "icons/female.png";
-  //     }
-
-  //     card.innerHTML = `
-  //       <img src="${item.photo}" alt="photo">
-  //       <div class="name">${item.name} ${item.surname}</div>
-  //       <div class="sex">
-  //         <img src=${icon} alt="male">
-  //       </div>
-  //       <div class="age">${item.age} age</div>
-  //     `;
-  //     document.querySelector('.app').appendChild(card);
-  //   });
-  // }
 });
