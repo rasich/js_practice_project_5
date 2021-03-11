08eb1b10276fa14bfcf6a7f25e246a34dd8c53b7
Nikolay <rasich16@gmail.com>
2021-03-11 17:59:33 +0200

    Отправка formData Axios

--- js/script.js
+++ js/script.js
@@ -5,25 +5,16 @@ window.addEventListener('DOMContentLoaded', () => {
     e.preventDefault();
 
     let formData = new FormData(form);
-
-    getResource("./api.php", formData)
-      .then(data => console.log(data))
-      .catch(err => console.error(err));
+    
+    axios({
+      method: 'post',
+      url: './api.php',
+      data: formData,
+      headers: {'content-type': 'multipart/form-data'}
+    })
+      .then(data => console.log(data.data));
+    
   }
 
   form.addEventListener('submit', (e) => req(e), {'once': true});
-
-  
-  async function getResource(url, data) {
-    const res = await fetch(`${url}`, {
-      method: "POST",
-      body: data
-    });
-
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-    }
-
-    return await res.text();
-  }
 });
