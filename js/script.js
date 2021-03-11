19c2380106784746d3e24d25d5d96701db520cb8
Nikolay <rasich16@gmail.com>
2021-03-11 17:33:29 +0200

    POST Fetch API

--- js/script.js
+++ js/script.js
@@ -11,23 +11,52 @@ window.addEventListener('DOMContentLoaded', () => {
     formData.forEach((value, key) => {
       obj[key] = value;
     });
-    let json = JSON.stringify(obj);
 
-    const request = new XMLHttpRequest();
-    request.open('POST', 'http://localhost:3000/people');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    request.send(json);
-
-    request.addEventListener('load', function() {
-      if (request.status == 200) {
-        let data = JSON.parse(request.response);
-        console.log(data);
-      } else {
-        console.error('Что-то пошло не так');
-      }
-    });
+    getResource("http://localhost:3000/people", obj)
+      // .then(data => createCards(data))
+      .catch(err => console.error(err));
   }
 
   form.addEventListener('submit', (e) => req(e), {'once': true});
+
+  async function getResource(url, data) {
+    const res = await fetch(`${url}`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+    
+    return await res.json();
+  }
   
+  // function createCards(response) {
+  //   response.forEach(item => {
+  //     let card = document.createElement('div');
+
+  //     card.classList.add('card');
+
+  //     let icon;
+  //     if (item.sex === 'male') {
+  //       icon = "icons/mars.png";
+  //     } else {
+  //       icon = "icons/female.png";
+  //     }
+
+  //     card.innerHTML = `
+  //       <img src="${item.photo}" alt="photo">
+  //       <div class="name">${item.name} ${item.surname}</div>
+  //       <div class="sex">
+  //         <img src=${icon} alt="male">
+  //       </div>
+  //       <div class="age">${item.age} age</div>
+  //     `;
+  //     document.querySelector('.app').appendChild(card);
+  //   });
+  // }
 });
