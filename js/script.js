733feed031466cf06cd3521816975d8715988259
Nikolay <rasich16@gmail.com>
2021-03-11 15:48:36 +0200

    Fetch API

--- js/script.js
+++ js/script.js
@@ -1,48 +1,51 @@
 window.addEventListener('DOMContentLoaded', () => { 
 
   function req() {
-    const request = new XMLHttpRequest();
-    request.open('GET', 'http://localhost:3000/people');
-    request.setRequestHeader('Content-type', 'app;ication/json; charset=utf-8');
-    request.send();
 
-    // request.addEventListener('readystatechange', function() {
-      // if (request.readyState === 4 && request.status == 200) {
-    request.addEventListener('load', function() {
-      if (request.status == 200) {
-        let data = JSON.parse(request.response);
-        console.log(data);
-
-        data.forEach(item => {
-          let card = document.createElement('div');
-
-          card.classList.add('card');
-
-          let icon;
-          if (item.sex === 'male') {
-            icon = "icons/mars.png";
-          } else {
-            icon = "icons/female.png";
-          }
-
-          card.innerHTML = `
-            <img src="${item.photo}" alt="photo">
-            <div class="name">${item.name} ${item.surname}</div>
-            <div class="sex">
-              <img src=${icon} alt="male">
-            </div>
-            <div class="age">${item.age} age</div>
-          `;
-          document.querySelector('.app').appendChild(card);
-        });
-      } else {
-        console.error('Что-то пошло не так');
-      }
-    });
+    // fetch('http://localhost:3000/people')
+    //   .then(data => data.json())
+    getResource('http://localhost:3000/people')
+      .then(data => createCards(data))
+      .catch(err => console.log(err));
 
     this.remove();
   }
 
   document.querySelector('button').addEventListener('click', req, {'once': true});
+
+  async function getResource(url) {
+    const res = await fetch(`${url}`);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+    
+    return await res.json();
+  }
+
+  function createCards(response) {
+    response.forEach(item => {
+      let card = document.createElement('div');
+
+      card.classList.add('card');
+
+      let icon;
+      if (item.sex === 'male') {
+        icon = "icons/mars.png";
+      } else {
+        icon = "icons/female.png";
+      }
+
+      card.innerHTML = `
+        <img src="${item.photo}" alt="photo">
+        <div class="name">${item.name} ${item.surname}</div>
+        <div class="sex">
+          <img src=${icon} alt="male">
+        </div>
+        <div class="age">${item.age} age</div>
+      `;
+      document.querySelector('.app').appendChild(card);
+    });
+  }
   
 });
